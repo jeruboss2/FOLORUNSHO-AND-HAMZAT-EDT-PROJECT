aaa2ce3b851a357751cce0d2b2920d47456f49d6
jeruboss2 <israelfolorunsho803@gmail.com>
2023-04-20 17:14:37 -0700

    feat: re-design home page

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-    // comment this is my first javascript code!
-    console.log('hello world');
+// //    // show menu on click
+// const navBtn = document.querySelector(".nav__menu");
+// const dropDown = document.querySelector(".nav__item");
+
+
+// navBtn.addEventListener("click", () => {
+//     dropDown.classList.toggle("show-dropdown");
+// });
     
+
+
+const navBtn = document.querySelector(".nav__item")
+const menuBtn = document.querySelector(".menu__btn");
+const closeBtn = document.querySelector(".close__btn");
+
+if (menuBtn) {
+  menuBtn.addEventListener("click", () => {
+    navBtn.classList.add("show__nav");
+    closeBtn.classList.add("show-Btn");
+    menuBtn.classList.add("show-menuBtn");
+  });
+}
+
+if (closeBtn) {
+  closeBtn.addEventListener("click", () => {
+    navBtn.classList.remove("show__nav");
+    closeBtn.classList.remove("show-Btn");
+    menuBtn.classList.remove("show-menuBtn");
+  });
+}
